c7292ef12fcdc7c466073ad9e223b7766f561e1c
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-09 01:59:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,15 +11,5 @@ end
 
 def countdown_with_sleep(x)
   y = x - 5
-  while x > 0
-    z = x # a temporary variable to store for the string
-    x -= 1
-    if x >= y
-      next
-    elsif x < y
-      puts "#{z} SECOND(S)!"
-    end
-    break if x == 0
-  end
-  return "HAPPY NEW YEAR!"
+  while(x >= )
 end
