2aa73033497cf518c7467aa968872a401bb978f1
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-09 01:38:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while(int > 0) do
+  int -= 1
+  puts "#{int} SECONDS!"
+  if int == 0
+    break
+  end
+  puts "Happy New Year!"
+end
 end
