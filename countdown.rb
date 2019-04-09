f0c6ea0faba24f25bcc681dcd58ebd65de520ec2
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-09 01:43:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,10 @@
 
 def countdown(int)
   while(int > 0) do
-    
-  str = "#{int} SECOND(S)!"
-  puts str
-  
-  int -= 1
-  break if int == 0
-    end
-    
+    str = "#{int} SECOND(S)!"
+    puts str
+    int -= 1
+    break if int == 0
   end
   puts "Happy New Year!"
 end
