39dc17edb795422e12cdea86078f46b31e60a16a
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-09 01:57:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,9 +12,9 @@ end
 def countdown_with_sleep(x)
   y = x - 5
   while x > 0
-    if(x >= y)
+    if x >= y
       next
-    elsif(x < y)
+    elsif x < y
       puts "#{x} SECOND(S)!"
     end
     break if x == 0
