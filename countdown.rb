27bac848362eb679364ec4ee4e9e907c950f191b
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-09 01:50:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,14 @@
 #write your code here
 
-def countdown(int)
-  
+def countdown(x)
+end
+
+def countdown_with_sleep(x)
+  y = x - 5
+  while(x >= 0)
+    if x > y
+      continue
+    elsif x < y
+      puts "#{x} SECOND(S)!"
+      end
 end
