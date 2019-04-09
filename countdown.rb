ccd83b6920bc8a74624fa947e2ee7907ddbd7623
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-09 01:52:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,12 +4,5 @@ def countdown(x)
 end
 
 def countdown_with_sleep(x)
-  y = x - 5
-  while(x >= 0)
-    str = "#{x} SECOND(S)!"
-    if x > y
-      continue
-    elsif x < y
-      puts str
-    end
+# 5 second delay
 end
